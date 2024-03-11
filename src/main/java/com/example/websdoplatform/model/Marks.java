2f9958a0894215ea7ae826cbb25bafc212f05f4b
Sergey Natarkin <snatarkin>
2024-03-11 15:54:28 +0300

    Uasdasd

--- src/main/java/com/example/websdoplatform/model/Marks.java
+++ src/main/java/com/example/websdoplatform/model/Marks.java
@@ -3,9 +3,12 @@ package com.example.websdoplatform.model;
 import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -19,10 +22,12 @@ public class Marks {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(name = "participant_studying_id")
-    private Long participantStudyingId;
-    @Column(name = "subject_id")
-    private Long subjectId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "participant_studying_id")
+    private ParticipantsStudying participantStudyingId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "subject_id")
+    private Subjects subjectId;
     private String mark;
     private Date date;
     private String comment;
